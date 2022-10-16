0be59565ed23ecd9c5f39dff1d4c063c01102419
kihhmaster <kihhmaster@yandex.ru>
2022-10-16 16:20:36 +0300

    Закончил верстку

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,21 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import '~/node_modules/jquery/dist/jquery.min.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+	document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+		$(".gallery").slice(0, 2).show().css('display', 'flex');
+		$("#loadMore").on("click", function(e){
+			e.preventDefault();
+			$(".gallery:hidden").slice(0, 1).css('display', 'flex').slideDown();
+			if($(".gallery:hidden").length == 0) {
+				$("#loadMore").fadeOut("slow");
+				// $("#loadMore").text("свернуть").addClass("noContent");
+				// $(".gallery:visible").slice(0, 2).show().css('display', 'flex');
+
+			}
+		});
+			
+		
 
 })
